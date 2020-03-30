7d3857590f6825bf7516dcf8c1f6d5282014cdcc
Gala <galamagdalena@hotmail.com>
2020-03-29 21:32:51 -0300

    Futbolista

---
+++ Futbolista/Futbolista/Futbolista.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbolista
+{
+    public class Futbolista
+    {
+        private string nombre;
+        private int edad;
+        private string posicion;
+        private double altura;
+        private int fuerza;
+        private int goles;
+        private int asistencias;
+
+
+
+        public Futbolista (string nombre, int edad, string posicion, double altura, int fuerza)          
+        {
+                this.nombre = nombre;
+                this.edad = edad;
+                this.posicion = posicion;
+                this.altura = altura;
+                this.fuerza = fuerza;
+                goles = 0;
+                asistencias = 0;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set { this.nombre = value; }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                return this.edad;
+            }
+            set { this.edad = value; }
+        }
+
+        public string Posicion
+        {
+            get
+            {
+                return this.posicion;
+            }
+            set { this.posicion = value; }
+        }
+
+        public double Altura
+        {
+            get
+            {
+                return this.altura;
+            }
+            set { this.altura = value; }
+        }
+
+        public int Fuerza
+        {
+            get
+            {
+                return this.fuerza;
+            }
+            set { this.fuerza = value; }
+        }
+
+        public void mostrarJugador()
+        {
+            Console.WriteLine("Nombre: " + Nombre + " Edad: " + Edad + " Posicion: " + Posicion + " Altura: " + Altura + " Fuerza: " + Fuerza);
+        }
+
+        public void correr()
+        {
+            Console.WriteLine("Estoy corriendo");
+        }
+
+        public void pasar()
+        {
+            Console.WriteLine("La estoy pasando");
+        }
+
+        public void patear()
+        {
+            Console.WriteLine("Patee al arco");
+
+            Console.WriteLine("Fue gol?: ");
+            string inputString;
+            inputString = Console.ReadLine();
+
+            if (inputString.ToLower() == "si")
+            {
+                goles = goles + 1;
+            }
+           
+        
+
+
+        }
+
+        public void mostrarCantGoles()
+        {
+            Console.WriteLine("Cantidad de goles: " + goles);
+        }
+
+
+        public void mostrarCantAsistencias()
+        {
+            Console.WriteLine("Cantidad de asistencias: " + asistencias);
+        }
+
+        public void asistencia()
+        {
+            Console.WriteLine("Hice una asistencia!");
+            asistencias = asistencias + 1;
+        }
+
+    }
+}
+
---
+++ Futbolista/Futbolista/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbolista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Futbolista miJugador = new Futbolista("Maradona", 33, "delantero", 1.65, 8);
+
+            miJugador.mostrarJugador();
+
+
+            miJugador.correr();
+
+            miJugador.pasar();
+
+            miJugador.mostrarCantGoles();
+
+            miJugador.mostrarCantAsistencias();
+
+            miJugador.asistencia();
+
+            miJugador.mostrarCantAsistencias();
+
+            miJugador.asistencia();
+
+            miJugador.mostrarCantAsistencias();
+
+            miJugador.patear();
+
+            miJugador.mostrarCantGoles();
+
+            miJugador.patear();
+
+            miJugador.mostrarCantGoles();
+
+            Console.ReadKey();
+        }
+    }
+}
